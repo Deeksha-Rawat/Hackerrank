3024ab41f0e44ecb4ac317669e32e340c5a4ddd7
Shubham <shubhamgupto@gmail.com>
2020-10-03 23:36:39 +0530

    [SQL] solutions for easy SQL questions in MySQL

---
+++ SQL/higher_than_75_marks.sql
@@ -0,0 +1,1 @@
+Select NAME FROM Students WHERE Marks > 75 ORDER BY RIGHT(Name,3), ID;
---
+++ SQL/weather_observation_station_8.sql
@@ -0,0 +1,5 @@
+select distinct city from station where city like 'a%%a' or city like 'a%%e' or city like 'a%%i' or city like 'a%%o' or city like 'a%%u'or 
+city like 'e%%a' or city like 'e%%e' or city like 'e%%i' or city like 'e%%o' or city like 'e%%u' or
+city like 'i%%a' or city like 'i%%e' or city like 'i%%i' or city like 'i%%o' or city like 'i%%u' or
+city like 'o%%a' or city like 'o%%e' or city like 'o%%i' or city like 'o%%o' or city like 'o%%u' or
+city like 'u%%a' or city like 'u%%e' or city like 'u%%i' or city like 'u%%o' or city like 'u%%u';
