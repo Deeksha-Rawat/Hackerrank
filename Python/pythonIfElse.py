652777ef81de3a088cf042afbbc5dff6d57622ec
shraddha-bhardwaj <shimar1bhardwaj0@gmail.com>
2020-10-04 18:43:38 +0530

    added a code for python If Else

---
+++ Python/pythonIfElse.py
@@ -0,0 +1,23 @@
+#https://www.hackerrank.com/challenges/py-if-else/problem
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    if n%2==0:
+        if n>=2 and n<=5:
+            print("Not Weird")
+        elif n>=6 and n<=20:
+            print("Weird")
+        elif n>20:
+            print("Not Weird")
+    else:
+        print("Weird")
+    
